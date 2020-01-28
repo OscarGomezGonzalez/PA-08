fb0bacc2a214577922a68bcfefcfdd4dec906e45
miguelgz97 <miguelgallego.z@gmail.com>
2020-01-28 02:11:46 +0100

    Modificaciones en Articulo
    creaciones, listar, etc..

--- Proyecto Netbeans/Pa-08/funciones.php
+++ Proyecto Netbeans/Pa-08/funciones.php
@@ -28,3 +28,17 @@ function conexionDB() {
     mysqli_set_charset($con, "utf-8");
     return $con;
 }
+
+function deleteDirectory($dir) {
+    if(!$dh = @opendir($dir)) return;
+    while (false !== ($current = readdir($dh))) {
+        if($current != '.' && $current != '..') {
+            echo 'Se ha borrado el archivo '.$dir.'/'.$current.'<br/>';
+            if (!@unlink($dir.'/'.$current)) 
+                deleteDirectory($dir.'/'.$current);
+        }       
+    }
+    closedir($dh);
+    echo 'Se ha borrado el directorio '.$dir.'<br/>';
+    @rmdir($dir);
+}
