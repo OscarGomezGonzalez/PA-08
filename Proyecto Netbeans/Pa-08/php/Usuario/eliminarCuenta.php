586f6b563e09fd42137f86931a09b7d354a5bbde
miguelgz97 <miguelgallego.z@gmail.com>
2020-01-29 23:49:21 +0100

    puliendo

--- Proyecto Netbeans/Pa-08/php/Usuario/eliminarCuenta.php
+++ Proyecto Netbeans/Pa-08/php/Usuario/eliminarCuenta.php
@@ -28,11 +28,11 @@ if (isset($_POST['SI'])) {
         ?>
         <div style="background-color: #c6c8ca; width: 45%; margin-left: 29%;  margin-top:15%; border: groove 1em red; border-radius: 2em;"><h3 class="text-danger" style="text-align: center;">¿Esta seguro de eliminar su cuenta?</h3></div>
         <div style="margin-top:2%; margin-left: 45%;" class="form-inline">
-            <form action="#"  method="POST">
+            <form action="php/Usuario/eliminarCuenta.php"  method="POST">
                 <button name="SI" type="submit" class="btn btn-danger"><i class="fas fa-pencil-alt d-xl-flex justify-content-xl-center align-items-xl-center">SI</i></button>
             </form>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
-            <form action="#"  method="POST">
+            <form action="php/Usuario/eliminarCuenta.php"  method="POST">
                 <button name="NO" type="submit" class="btn btn-warning"><i class="far fa-trash-alt d-xl-flex justify-content-xl-center align-items-xl-center">NO</i></button>
             </form>
         </div>
